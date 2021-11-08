25131ba54f28515fa2dc0045df864ff1c76471d1
Ben S <benslinde@gmail.com>
2021-11-08 13:10:00 -0500

    completed skills and resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,22 +1,80 @@
 import React from 'react';
 
+const styles = {
+  resumeStyle: {
+    fontColor: '#717E8E',
+    borderStyle: 'solid',
+    borderColor: '#717E8E',
+    borderRadius: '10px',
+    marginTop: '10px',
+  },
+  textStyle: {
+    fontColor: '#717E8E',
+    textAlign: 'center',
+  },
+  headStyle: {
+    marginTop:'10px',
+  },
+  hrStyle: {
+    borderTop: '10px solid black',
+  },
+  image: {
+    height: '200px',
+    width: '200px',
+    borderRadius: '10px',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderStyle: 'solid',
+    borderColor: '#717E8E',
+  },
+  imgContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: '25px',
+  }
+};
+
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume Page</h1>
-      <h1> Need  skill set list</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div style={styles.resumeStyle} className="container">
+      <h1 style={styles.headStyle}>Skills and Resume</h1>
+      <hr style={styles.hrStyle}/>
+      <div className="row">
+
+        <div className="col-4">
+          <ul>
+            <li>React.js</li>
+            <li>JavaScript</li>
+            <li>Express.js</li>
+            <li>Node.js</li>
+            <li>MongoDB</li>
+            <li>MySQL</li>
+            <li>jQuery</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>PWA</li>
+            <li>Agile Methodoloy</li>
+            <li>Git</li>
+          </ul>
+        </div>
+
+      
+        <div style={styles.imgContainer} className="col-xs-12 col-md-8">
+          <div className="row">
+            <div className="col-12">
+              <h3 style={styles.textStyle}>
+                Consider my Resume.
+              </h3>
+            </div>
+            <div className="col-12">
+            <a href="./assets/pdf/BS-SE-10082012.pdf"><img style={styles.image} className="" src={"./assets/images/ssbsresume.png"} alt=" of Ben Slinde's resume" /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
