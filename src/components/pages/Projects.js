fdc16e530db2b6d59399578323135042fd6edb12
Ben S <benslinde@gmail.com>
2021-11-09 11:40:46 -0500

    portfolio page complete

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -13,31 +13,16 @@ const styles = {
     marginRight: '5px',
     background: 'black',
   },
-  textStyle: {
-    fontColor: '#717E8E',
-    textAlign: 'center',
-  },
   headStyle: {
     marginTop:'10px',
     textAlign: 'center',
   },
-  hrStyle: {
-    borderTop: '10px solid black',
+  imageContStyle: {
+    minHeight: '200px',
+    minWidth: '200px',
   },
-  image: {
-    height: '200px',
-    width: '200px',
+  imgStyle: {
     borderRadius: '10px',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    borderStyle: 'solid',
-    borderColor: '#717E8E',
-  },
-  imgContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginBottom: '25px',
   },
   colorGrey: {
     color: '#717E8E',
@@ -55,33 +40,39 @@ export default function Projects() {
       <h1 style={styles.headStyle}>Showcased Projects</h1>
       <div className="row d-flex justify-content-evenly">
         <div className="card" style={styles.cardStyle}>
-          <img src="./assets/images/screenshothome.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
-          <div className="card-body">
+          <div style={styles.imageContStyle}>
+            <img style={styles.imgStyle} src="./assets/images/screenshothome.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
+          </div>
+          <div className="card-body d-flex align-items-start flex-column">
            <h4 style={styles.colorGrey} className="card-title font-weight-bold">Book Club Collective</h4>
             <p style={styles.colorGrey} className="card-text">This app allows the user to make a profile, leave book reviews, read reviews left by other users and create a club for other users to join to discuss books.</p>
-              <div className="row">
+              <div className="row mt-auto">
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Repo" href="https://github.com/stevenslade/Book-Club-Collective"><i style={styles.iconStyle} className="bi bi-github"></i></a>
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Deployed App" href="https://intense-tor-64466.herokuapp.com/"><i style={styles.iconStyle} className="bi bi-gear-fill"></i></a>
               </div>
           </div>
         </div>
         <div className="card" style={styles.cardStyle}>
-          <img src="./assets/images/HHSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
-          <div className="card-body">
+          <div style={styles.imageContStyle}>
+            <img style={styles.imgStyle} src="./assets/images/HHSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
+          </div>
+          <div className="card-body d-flex align-items-start flex-column">
             <h4 style={styles.colorGrey} className="card-title font-weight-bold">Hiking Hub</h4>
             <p style={styles.colorGrey} className="card-text">An app that enables the user to search for local hiking trails and then find trail related content on reddit.</p>
-              <div className="row">
+              <div className="row mt-auto">
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Repo" href="https://github.com/stevenslade/Hiking-Hub"><i style={styles.iconStyle} className="bi bi-github"></i></a>
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Deployed App" href="https://stevenslade.github.io/Hiking-Hub/"><i style={styles.iconStyle} className="bi bi-gear-fill"></i></a>
               </div>
           </div>
         </div>
         <div className="card" style={styles.cardStyle}>
-          <img src="./assets/images/weatherDashboardSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
-          <div className="card-body">
+          <div style={styles.imageContStyle}>
+            <img style={styles.imgStyle} src="./assets/images/weatherDashboardSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
+          </div>
+          <div className="card-body d-flex align-items-start flex-column">
             <h4 style={styles.colorGrey} className="card-title font-weight-bold">Weather Dashboard</h4>
             <p style={styles.colorGrey} className="card-text">A weather forecasting app which allows the user to select a city and see the current weather and a five day forecast.</p>
-              <div className="row">
+              <div className="row mt-auto">
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Repo" href="https://github.com/stevenslade/WeatherDashboard"><i style={styles.iconStyle} className="bi bi-github"></i></a>
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Deployed App" href="https://stevenslade.github.io/WeatherDashboard/"><i style={styles.iconStyle} className="bi bi-gear-fill"></i></a>
               </div>
@@ -90,33 +81,39 @@ export default function Projects() {
       </div>
     <div className="row d-flex justify-content-evenly">
       <div className="card" style={styles.cardStyle}>
-        <img src="./assets/images/SSBW.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
-          <div className="card-body">
+        <div style={styles.imageContStyle}>
+          <img style={styles.imgStyle} src="./assets/images/SSBW.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
+        </div>
+          <div className="card-body d-flex align-items-start flex-column">
             <h4 style={styles.colorGrey} className="card-title font-weight-bold">Travel Budget Tracker</h4>
             <p style={styles.colorGrey} className="card-text">A simple budget tracker allowing the user to enter positive and negative fund allocations to see a total remaining balance.</p>
-              <div className="row">
+              <div className="row mt-auto">
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Repo" href="https://github.com/stevenslade/Budget-Tracker"><i style={styles.iconStyle} className="bi bi-github"></i></a>
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Deployed App" href="https://stark-reaches-88697.herokuapp.com/"><i style={styles.iconStyle} className="bi bi-gear-fill"></i></a>
               </div>
           </div>
         </div>
         <div className="card" style={styles.cardStyle}>
-          <img src="./assets/images/TTSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
-          <div className="card-body">
+          <div style={styles.imageContStyle}>
+            <img style={styles.imgStyle} src="./assets/images/TTSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
+          </div>
+          <div className="card-body d-flex align-items-start flex-column">
            <h4 style={styles.colorGrey} className="card-title font-weight-bold">Tech Blog</h4>
-            <p style={styles.colorGrey} className="card-text">This app allows the user to make a profile, leave book reviews, read reviews left by other users and create a club for other users to join to discuss books.</p>
-              <div className="row">
+            <p style={styles.colorGrey} className="card-text">This app allows the user to create a login and then leave blog posts.  Signed in users can also respond to posts created by other users.</p>
+              <div className="row mt-auto">
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Repo" href="https://github.com/stevenslade/Tech-Theory"><i style={styles.iconStyle} className="bi bi-github"></i></a>
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Deployed App" href="https://arcane-forest-46584.herokuapp.com/"><i style={styles.iconStyle} className="bi bi-gear-fill"></i></a>
               </div>
           </div>
         </div>
         <div className="card" style={styles.cardStyle}>
-          <img src="./assets/images/WorkDaySchedulerSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
-          <div className="card-body">
+          <div style={styles.imageContStyle}>
+            <img style={styles.imgStyle} src="./assets/images/WorkDaySchedulerSS.png" class="card-img-top mt-2" alt="book shelf and cards of book reviews" />
+          </div>
+          <div className="card-body d-flex align-items-start flex-column">
             <h4 style={styles.colorGrey} className="card-title font-weight-bold">Work Day Scheduler</h4>
-            <p style={styles.colorGrey} className="card-text">This app allows the user to make a profile, leave book reviews, read reviews left by other users and create a club for other users to join to discuss books.</p>
-              <div className="row">
+            <p style={styles.colorGrey} className="card-text">A single day task planning app. Allows entry and edits of tasks scheduled per hour.  Rows change color to indicate past, present or future time slot. </p>
+              <div className="row mt-auto">
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Repo" href="https://github.com/stevenslade/Homework-GT-FSF-PT-06-2021/tree/main/05-Third-Party-APIs-Work-Day-Scheduler"><i style={styles.iconStyle} className="bi bi-github"></i></a>
                 <a className="col" data-bs-toggle="tooltip" data-bs-placement="top" title="Click to see the Deployed App" href="https://stevenslade.github.io/Homework-GT-FSF-PT-06-2021/05-Third-Party-APIs-Work-Day-Scheduler/"><i style={styles.iconStyle} className="bi bi-gear-fill"></i></a>
                 </div>
